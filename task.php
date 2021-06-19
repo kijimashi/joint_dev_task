ef1e0df7975b71d62abbd019a76a25f9051a8958
kijimashi <jzx100kai_8020@icloud.com>
2021-06-19 17:50:09 +0900

    php基礎1-4

--- task.php
+++ task.php
@@ -1,124 +1,134 @@
+<?php
 # 課題の回答は このファイル をご利用下さい。
 # 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
 
-print("#####q1#####".PHP_EOL);
+print("#####q1#####" . PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
-  # 以下に回答を記載
-
+# 以下に回答を記載
+array_push($names, "斎藤");
+print_r($names);
 echo PHP_EOL;
 
-print("#####q2#####".PHP_EOL);
+print("#####q2#####" . PHP_EOL);
 $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
-  # 以下に回答を記載
-
+# 以下に回答を記載
+$array = array_merge($array1, $array2);
+print_r($array);
 echo PHP_EOL;
 
-print("#####q3#####".PHP_EOL);
+print("#####q3#####" . PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
 
-  # 以下に回答を記載
-
+# 以下に回答を記載
+$count = 0;
+foreach ($numbers as $number) {
+  if ($number == 3) {
+    $count++;
+  }
+}
+print_r($count . "個" . PHP_EOL);
 echo PHP_EOL;
 
-print("#####q4#####".PHP_EOL);
+print("#####q4#####" . PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
-  # 以下に回答を記載
-
+# 以下に回答を記載
+$sports = array_diff($sports, [null]);
+print_r($sports);
 echo PHP_EOL;
 
-print("#####q5#####".PHP_EOL);
+print("#####q5#####" . PHP_EOL);
 $array1 = [];
 $array2 = [1, 5, 8, 10];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q6#####".PHP_EOL);
+print("#####q6#####" . PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q7#####".PHP_EOL);
+print("#####q7#####" . PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
-  # 以下は変更しないで下さい
+# 以下は変更しないで下さい
 var_dump($array);
 
 echo PHP_EOL;
 
-print("#####q8#####".PHP_EOL);
+print("#####q8#####" . PHP_EOL);
 $programming_languages = ["php", "ruby", "python", "javascript"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
-  # 以下は変更しないで下さい
+# 以下は変更しないで下さい
 print_r($programming_languages);
 echo PHP_EOL;
 print_r($upper_case_programming_languages);
 
 echo PHP_EOL;
 
-print("#####q9#####".PHP_EOL);
+print("#####q9#####" . PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q10#####".PHP_EOL);
+print("#####q10#####" . PHP_EOL);
 $foods = ["いか", "たこ", "うに", "しゃけ", "うにぎり", "うに軍艦", "うに丼", "高級なうに"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q11#####".PHP_EOL);
+print("#####q11#####" . PHP_EOL);
 $sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q12#####".PHP_EOL);
+print("#####q12#####" . PHP_EOL);
 $data = ["user" => ["name" => "satou", "age" => 33]];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q13#####".PHP_EOL);
+print("#####q13#####" . PHP_EOL);
 $user_data = ["name" => "神里", "age" => 31, "address" => "埼玉"];
 $update_data = ["age" => 32, "address" => "沖縄"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q14#####".PHP_EOL);
+print("#####q14#####" . PHP_EOL);
 $data = ["name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q15#####".PHP_EOL);
+print("#####q15#####" . PHP_EOL);
 $data1 = ["name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin"];
 $data2 = ["name" => "yamada", "hobby" => "baseball", "role" => "normal"];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q16#####".PHP_EOL);
+print("#####q16#####" . PHP_EOL);
 $users = [
   ["name" => "satou", "age" => 22],
   ["name" => "yamada", "age" => 12],
@@ -126,11 +136,11 @@ $users = [
   ["name" => "nakamura", "age" => 41]
 ];
 
-  # 以下に回答を記載
+# 以下に回答を記載
 
 echo PHP_EOL;
 
-print("#####q17#####".PHP_EOL);
+print("#####q17#####" . PHP_EOL);
 class User
 {
 
@@ -142,14 +152,14 @@ $user1 = new User("神里", 32, "男");
 $user2 = new User("あじー", 32, "男");
 
 $user1->info();
-print("-------------".PHP_EOL);
+print("-------------" . PHP_EOL);
 $user2->info();
 
 echo PHP_EOL;
 
-print("#####q18#####".PHP_EOL);
+print("#####q18#####" . PHP_EOL);
 
-  # コードを追加
+# コードを追加
 
 $man1 = new Man("あじー", 32);
 $man2 = new Man("ゆたぼん", 10);
@@ -159,24 +169,26 @@ $man2->introduce();
 
 echo PHP_EOL;
 
-print("#####q19#####".PHP_EOL);
-class Item{
+print("#####q19#####" . PHP_EOL);
+class Item
+{
   # 以下を修正して下さい
 
   protected $name;
 
-  function __construct($book_name){
+  function __construct($book_name)
+  {
     $this->name = $book_name;
   }
 }
-  # 以下は変更しないで下さい
+# 以下は変更しないで下さい
 
 $book = new Item("ゼロ秒思考");
-print($book->name.PHP_EOL);
+print($book->name . PHP_EOL);
 
 echo PHP_EOL;
 
-print("#####q20#####".PHP_EOL);
+print("#####q20#####" . PHP_EOL);
 class Human
 {
 
@@ -191,7 +203,7 @@ class Zoo
 
 }
 
-$zoo = new Zoo("旭山動物園",["infant" => 0, "children" => 400, "adult" => 800, "senior" => 500]);
+$zoo = new Zoo("旭山動物園", ["infant" => 0, "children" => 400, "adult" => 800, "senior" => 500]);
 
 $human1 = new Human("たま", 3);
 $human2 = new Human("ゆたぼん", 10);
@@ -200,7 +212,7 @@ $human4 = new Human("ぎん", 108);
 
 $humans = [$human1, $human2, $human3, $human4];
 
-foreach($humans as $human){
+foreach ($humans as $human) {
   $zoo->info_entry_fee($human);
 }
 
